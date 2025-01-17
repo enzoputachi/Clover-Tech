5bb9cf1d684f80c274e2c1756b6571996a908a3c
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-17 06:57:36 +0100

    create an axios instance

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,1 +1,10 @@
-import 
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "http://localhost:4200",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export default api;
