d72f3825a818cabc8fb43407416d51c3392bec81
Enzoputachi <samuelnzoputachi@gmail.com>
2025-02-03 10:44:35 +0100

    Udate code

--- src/components/cart/CartProvider.tsx
+++ src/components/cart/CartProvider.tsx
@@ -6,11 +6,12 @@ import api from '@/api/api';
 interface CartContextType {
   items: Course[];
   addItem: (course: Course) => void;
-  removeItem: (courseId: string) => void;
+  removeItem: (courseId: string, price: number) => void;
   total: number;
   clearCart: () => void;
   fetchCart: () => void;
 }
+
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
@@ -23,10 +24,11 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   const fetchCart = async () => {
     try {
       const res = await api.get<{ items: Course[], total: number }>("/api/cart");
-      setItems(res.data.items)
-      setTotal(res.data.total)
+      setItems(res.data?.items)
+      setTotal(res.data?.total)
+      console.log(res.data.items);
     } catch (error) {
-      console.error("Error fetching cart:", error?.message);
+      console.error("Error fetching cart:", error.response?.data || error.message);
     }
   };
 
@@ -35,31 +37,43 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   const addItem = async (course: Course) => {
+    console.log('COURSE 4RM CART:', course);
+    
+    if (!course || !course._id) {
+      console.error("Invalid course object passed to addItem:");
+      return;
+    }
+
     try {
-        await api.post('/api/cart/add', { courseId: course.id})
+      // console.log('COURSE IN CART:', course.id);
+      
+        await api.post('/api/cart/add', { courseId: course._id, price: course.price })
         toast({
           title: "Course added to cart",
           description: `${course.title} has been added to your cart.`
         });
         fetchCart();
     } catch (error) {
-      toast({
-        title: "Course already in cart",
-        description: "This course is already in your cart.",
-        variant: "destructive"
-      })
+      console.error("Error adding couse to cart:", error?.message || error);
     }
   };
 
-  const removeItem = async (courseId: string) => {
+  const removeItem = async (courseId: string, price: number) => {
+    console.log("PRICE:", price);
+    console.log("COURSE ID:", courseId);
+    
     try {
-      await api.delete(`/api/cart/${courseId}`)
+      await api.delete(`/api/cart/${courseId}`, { 
+        data: { price }
+      })
       toast({
         title: "Course removed",
         description: "The course has been removed from your cart."
       });
       fetchCart()
     } catch (error) {
+      console.error("Error removing course:", error?.message || error);
+      
       toast({ 
         title: "Error", 
         description: error.response?.data?.message || "Failed to remove course", 
