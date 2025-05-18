6c009c9ce4b548e03fabd6e91828e98ab235bf58
Enzoputachi <samuelnzoputachi@gmail.com>
2025-05-18 08:51:27 +0100

    update name

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:4200",
+    baseURL: "https://clover-website-lms-sever.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
